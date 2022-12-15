4ca125f4606378e1c491b57e8a7b8855641560ff
Sartosh-B <85825443+Sartosh-B@users.noreply.github.com>
2022-12-15 22:45:33 +0100

    Weapon_Zoom_Controller_Added

---
+++ Assets/Script/WeaponZoom.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class WeaponZoom : MonoBehaviour
+{
+    [SerializeField] Camera fpsCamera;
+    [SerializeField] float zoomInFOV = 30.0f;
+    [SerializeField] float zoomOutFOV = 60.0f;
+
+    [SerializeField] float zoomOutSensitivity = 2.0f;
+    [SerializeField] float zoomInSensitivity = 1.0f;
+
+
+    RigidbodyFirstPersonController fpsController;
+
+    bool zoomedInToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedInToggle == false)
+            {
+                zoomedInToggle = true;
+                fpsCamera.fieldOfView = zoomInFOV;
+                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+            }
+            else
+            {
+                zoomedInToggle = false;
+                fpsCamera.fieldOfView = zoomOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+            }           
+        }
+    }    
+}
